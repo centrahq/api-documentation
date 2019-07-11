c5ffcc2ccd9f95c8380b3141050bf374fdab125d
Nataliia Kononikhina <nataliia.kononikhina@centra.com>
2019-07-11 10:21:31 +0200

    feat: Add Step sord code. Add Success notice

---
+++ user/shortcodes/StepShortcode.php
@@ -0,0 +1,16 @@
+<?php
+namespace Grav\Plugin\Shortcodes;
+
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+
+class StepShortcode extends Shortcode
+{
+    public function init()
+    {
+        $this->shortcode->getHandlers()->add('step', function(ShortcodeInterface $sc) {
+            $this->shortcode->addAssets('css', 'theme://css/style.css');
+            $step = $sc->getParameter('step', $this->getBbCode($sc));
+            return '<br/><span class="step">'.$step.'</span>';
+        });
+    }
+}
